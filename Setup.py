216731db8523ddd6ea04578ffb4b5d88330201f3
Amixr3 <139391120+Amixr3@users.noreply.github.com>
2023-09-05 21:08:31 +0330

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,12 +1,12 @@
 n = int(input("1=make picture|2=instainfo|3=logo |4=chatgbt|با توجه به نیازتون وارد کنید"))
 
 if n == 1:
-    exec(open('code1.py').read())
+    exec(open('amirbot1.py').read())
 elif n == 2:
-    exec(open('code2.py').read())
+    exec(open('amirbot2.py').read())
 elif n == 3:
-    exec(open('code3.py').read())
+    exec(open('amirbot3.py').read())
 elif n == 4:
-    exec(open('code4.py').read())
+    exec(open('amirbot4.py').read())
 else:
     print("Invalid number entered.")
